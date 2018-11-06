8e538878fcdd58842f8e388cd0eb914820ad1ff0
Justin Study <js4050@ship.edu>
2018-11-06 03:12:28 -0500

    implemented world renderer

--- core/src/main/Main.java
+++ core/src/main/Main.java
@@ -1,17 +1,16 @@
 package main;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 import game.Assets;
 import game.WorldController;
-import screens.GameScreen;
+import game.WorldRenderer;
 
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
@@ -40,6 +39,8 @@ public class Main implements ApplicationListener
 		worldController.update(Gdx.graphics.getDeltaTime());
 		//Sets screen to sky blue
 		Gdx.gl.glClearColor(0x64/255.0f,  0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		//clears the screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		//render game world to screen
 		worldRenderer.render();
