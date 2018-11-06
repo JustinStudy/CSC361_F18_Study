d787cd2b5eccbdca44ac68cfa83cde0cb27d45f6
Justin Study <js4050@ship.edu>
2018-11-06 01:17:17 -0500

    added world controller code

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,12 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
-import main.CSC361_F18_Study;
+import main.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CSC361_F18_Study(), config);
+		new LwjglApplication(new Main(), config);
 	}
 }
