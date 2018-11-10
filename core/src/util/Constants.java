175879c631b2cb194697a66874dbc2708538627e
Justin Study <js4050@ship.edu>
2018-11-10 04:26:11 -0500

    level code added to worldcontroller

--- core/src/util/Constants.java
+++ core/src/util/Constants.java
@@ -13,9 +13,18 @@ public class Constants
 	//Visible game world is 7 meters tall
 	public static final float VIEWPORT_HEIGHT = 5.0f;
 	
+	//GUI width
+	public static final float VIEWPORT_GUI_WIDTH = 800.0f;
+	
+	//GUI height
+	public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
+	
 	//Location of description file for texture atlas
 	public static final String TEXTURE_ATLAS_OBJECTS = "images/Study.pack";
 	
 	//Location of image file for level
 	public static final String LEVEL_01 = "levels/level.png";
+	
+	//amount of extra lives at level start
+	public static final int LIVES_START = 3;
 }
